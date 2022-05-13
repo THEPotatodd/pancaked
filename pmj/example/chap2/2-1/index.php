ad44c0667a5f95d7d89a3847d78cc15a771d864b
THEPotatodd <105214486+THEPotatodd@users.noreply.github.com>
2022-05-13 19:25:45 +0700

    การเขียนคำอธิบาย

---
+++ pmj/example/chap2/2-1/index.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<h3>
+<?php
+echo "PHP/MySQL/jQuery";
+?>
+</h3>
+<div style="font-size:larger; color:blue;">
+<?php
+echo "<b>สิ่งที่ต้องติดตั้ง:</b>;
+?>
+</div>
+<ul>
+<?php
+echo "<li>Apache</li>";
+echo "<li>PHP</li>";
+echo "<li>MySQL</li>";
+?>
+</ul>
+</body>
+</html>
